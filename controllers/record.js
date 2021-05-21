7a6df5730809ce02bf205bd4d8515a7ce929c4ae
pneumoniadetector <84466091+pneumoniadetector@users.noreply.github.com>
2021-05-21 20:56:06 +0530

    Update record.js

--- controllers/record.js
+++ controllers/record.js
@@ -24,3 +24,46 @@ exports.getRecord = (req, res) => {
     })
   })
 }
+
+
+exports.sendRecord=(req,res)=>{
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.user, 
+      pass: process.env.pass, 
+    },
+  });
+  let table=`<table style=" width:450px;margin:0;" cellpadding="15" cellspacing="1" border="2">
+  <tr>
+    <th>Name</th>
+    <th>Results</th>
+    <th>Photo</th>
+  </tr>
+  <tr>
+    <td>${req.body.name}</td>
+    <td>${req.body.result}</td>
+    <td><img src='${req.body.url}' width='100' height='100' /></td>
+  </tr>
+</table>`
+  const mailOptions = {
+    from: `Pneumonia detector <${process.env.user}>`, // sender address
+    to: req.body.emailto, 
+    subject: req.body.subject,
+    html:table
+
+  };
+  
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions,(err,info)=>{
+      if(err){
+          console.log(err)
+      }else{
+          console.log('email sent success')
+          res.status(200).json({
+            info
+          })
+      }
+  });
+}
