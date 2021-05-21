c8768d7346231ec48bed1e4b6bd54675e2f31579
pneumoniadetector <84466091+pneumoniadetector@users.noreply.github.com>
2021-05-21 20:57:17 +0530

    Update record.js

--- routes/record.js
+++ routes/record.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const { addRecord, getRecord } = require("../controllers/record");
+const { addRecord, getRecord ,sendRecord} = require("../controllers/record");
 
 router.post(
   "/addRecord",
@@ -15,6 +15,5 @@ router.post(
   "/sendRecord",
   sendRecord
  );
-
 
 module.exports = router;
